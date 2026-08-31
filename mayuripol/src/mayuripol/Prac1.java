8722f4b7c747fb12938c70ed0b0412e53f8dd3cc
shubham <shubham@LAPTOP-NVJG7KRT>
2026-08-31 18:04:09 +0530

    this is commit

---
+++ mayuripol/src/mayuripol/Prac1.java
@@ -0,0 +1,13 @@
+package mayuripol;
+
+public class Prac1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Core Java..");
+		
+	}
+
+}
